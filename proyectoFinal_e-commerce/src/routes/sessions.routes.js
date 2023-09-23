af4c1196a50147e421109b2d6c195a09ae64a9e5
Sanchez Nicolas <sancheznicolasalejandro@gmail.com>
2023-09-23 16:45:48 -0300

    Desafio login version v2.21

--- proyectoFinal_e-commerce/src/routes/sessions.routes.js
+++ proyectoFinal_e-commerce/src/routes/sessions.routes.js
@@ -5,34 +5,49 @@ const sessionRouter = Router()
 
 sessionRouter.post('/login', async (req, res) => {
     const { email, password } = req.body
-
     //console.log("que es: ",req.session.login)
-
     try {
-        if (req.session.login)
+        if (req.session.login){
             res.status(200).send({ resultado: 'Login ya existente' })
+            return;
+        }
+        if (email === 'nico@nico.com' && password === 'nico') {
+			req.session.login = true;
+			req.session.user = {
+				first_name: 'Nico',
+				last_name: 'Nico',
+				age: 38,
+				email: email,
+				rol: 'admin',
+			};
+			res.redirect('../../static/realTimeProducts');
+			return;
+		}
         const user = await userModel.findOne({ email: email })
-
         if (user) {
             if (user.password == password) {
-                req.session.login = true
-                res.status(200).send({ resultado: 'Login valido', message: user })
-                //res.redirect('ruta', 200, {'info': user}) Redireccion
+				req.session.login = true;
+				req.session.user = {
+					first_name: user.first_name,
+					last_name: user.last_name,
+					age: user.age,
+					email: user.email,
+					rol: user.rol,
+				};
+				res.redirect('../../static/realTimeProducts');
             } else {
-                res.status(401).send({ resultado: 'Unauthorized', message: user })
-            }
-        } else {
-            res.status(404).send({ resultado: 'Not Found', message: user })
-        }
-    } catch (error) {
+				res.status(400).send({resultado: 'constraseña incorrecta'
+				});
+			}
+        }else{
+            res.status(400).send({resultado: 'email incorrecto'})
+            } 
+        }catch (error) {
+            console.error("Error en sessionRouter post:", error);
+            res.status(500).send({ error: "Error interno del servidor" });
         res.status(400).send({ error: `Error en login: ${error}` })
     }
 })
-
-
-
-
-
 sessionRouter.get('/logout', (req, res) => {
     if (req.session.login) {
         req.session.destroy()
