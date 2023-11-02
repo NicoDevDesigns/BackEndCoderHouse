8ab21e87e273ca1513473c312665b045abd580fb
Sanchez Nicolas <sancheznicolasalejandro@gmail.com>
2023-11-02 15:56:40 -0300

    Tercera pre-entrega v2

--- proyectoFinal_e-commerce/src/public/js/login.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('signIn').addEventListener('click', async() => {
-    Swal.fire({title: 'Crear Usuario'}).then(() => {
-            window.location.href = '/static/signInUser';
-        });
-});
